3d8826ec6883ea2d14db5e6b8b9a201e031bb377
Laura Silva Lopes <lauraslopes94@gmail.com>
2019-05-21 18:41:33 -0300

    VRI 21-05

--- calibration.py
+++ calibration.py
@@ -12,37 +12,38 @@ imgpoints = [] # 2d points in image plane.
 cap = cv.VideoCapture(0)	
 actualFunction = 0
 
-def function_01:
-    global imgpoints
-    global objpoints
-    global img
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners)
-        # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
-        cv.imshow('img', img)
+def function_01():
+	global imgpoints
+	global objpoints
+	global img
+	global gray
+	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+	# Find the chess board corners
+	ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+	# If found, add object points, image points (after refining them)
+	if ret == True:
+		objpoints.append(objp)
+		corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+		imgpoints.append(corners)
+		# Draw and display the corners
+		cv.drawChessboardCorners(img, (7,6), corners2, ret)
+	cv.imshow('img', gray)
 
 
-def function_02:
-    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-    cv.imshow('img', dst)
+def function_02():
+	dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+	cv.imshow('img', dst)
 
 
 functions = [function_01, function_02]
 while(1):
-    _, img = cap.read()
-    functions[actualFunction]()
-    k = cv2.waitKey(34)
-    if k == 27:
-        actualFunction += 1
-	ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-	newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-    if actualFunction == 2:
-        break
+	_, img = cap.read()
+	functions[actualFunction]()
+	k = cv.waitKey(34)
+	if k == 27:
+		actualFunction += 1
+		ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+		newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+	if actualFunction == 2:
+		break
 cv.destroyAllWindows()
